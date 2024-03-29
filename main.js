165235671e82b21f069f13565471bd72f04e6788
DeadCodeBoy <14.02.85@mail.ru>
2024-03-29 18:05:58 +0300

    second commit
    second

--- main.js
+++ main.js
@@ -21,14 +21,7 @@ class Gallary{
         this.gallary = document.getElementById(options.root); 
         this.gallaryItems = this.createElement('div', {className: options.selectorItems});
         this.wrapper = this.createElement('div', {className: options.wrapper});
-        this.buttonNext = this.createElement('button', {
-            className: 'next',
-           // text: 'next',
-           // scr: '/images/next.png',
-            attr: {
-                ['data-btn']: 'next',
-            }
-        });
+       
         this.buttonPrev = this.createElement('button', {
             className: 'prev',
            // text: 'prev',
@@ -38,6 +31,15 @@ class Gallary{
             }
         
         }); 
+
+        this.buttonNext = this.createElement('button', {
+            className: 'next',
+           // text: 'next',
+           // scr: '/images/next.png',
+            attr: {
+                ['data-btn']: 'next',
+            }
+        });
         // TODO: ! навесить на кнопки события
         this.items = options.gallaryItems.items.map(item => {
             return this.createElement('img', {
@@ -52,8 +54,8 @@ class Gallary{
         this.gallary.appendChild(this.gallaryItems);
         this.gallaryItems.appendChild(this.wrapper);
         this.wrapper.append(...this.items);
-        this.gallary.append(this.buttonNext, this.buttonPrev);
-        console.log(this);
+        this.gallary.append(this.buttonPrev, this.buttonNext);
+       // console.log(this);
     }
 
     createElement(tagName, options) {
@@ -78,12 +80,21 @@ class Gallary{
     }
 
     initListeners() {
-        debugger
+        //debugger
         this.gallary.addEventListener('click', (event) => {
-            console.log(event.target);
-            if(event.target.dataset?.btn === 'next' ) this.next();
+           // console.log(event.target);
             if(event.target.dataset?.btn === 'prev' ) this.prev();
+            if(event.target.dataset?.btn === 'next' ) this.next();
         })
+    }
+    prev() {
+        if(this.slideIndex  !== 0) {
+            this.slideIndex -= 1
+        }else{
+            this.slideIndex = this.slideLength - 1;
+        }
+        this.move();
+        
     }
         
     next() {
@@ -97,15 +108,7 @@ class Gallary{
         this.move();
     }
 
-    prev() {
-        if(this.slideIndex  !== 0) {
-            this.slideIndex -= 1
-        }else{
-            this.slideIndex = this.slideLength - 1;
-        }
-        this.move();
-        
-    }
+    
       
     move() {
         this.wrapper.style.transform = `translateX(${-this.stepPercent * this.slideIndex}%)`;
@@ -129,3 +132,33 @@ class Gallary{
     }
 
   })
+
+  class Faq {
+    activeElement = null;
+
+    constructor(selector) {
+        this.init(selector);
+        this.listeners();
+    }
+
+    init(selector) {
+       this.faq = document.querySelector(selector);
+    }
+
+    listeners() {
+        this.faq.addEventListener('click', (event) => {
+            const target = event.target;
+            const dataTarget = target.getAttribute('data-item'); 
+            if(dataTarget) {
+                if(this.activeElement && event.target !== this.activeElement) {
+                this.activeElement.classList.remove('active');
+                
+            }
+            target.classList.add('active');
+            this.activeElement = target;
+        }
+    
+        })
+    }
+}
+new Faq('.accordion');
