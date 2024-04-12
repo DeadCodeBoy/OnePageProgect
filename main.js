ec9600c49214e17ae99123cfda52fe7974209f29
DeadCodeBoy <14.02.85@mail.ru>
2024-04-12 20:42:28 +0300

    12.04.2024
    almost done

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ class Gallary {
         ['data-btn']: 'next',
       },
     });
-    // TODO: ! навесить на кнопки события
+   
     this.items = options.gallaryItems.items.map((item) => {
       return this.createElement('img', {
         src: item,
@@ -50,8 +50,7 @@ class Gallary {
     this.gallary.appendChild(this.gallaryItems);
     this.gallaryItems.appendChild(this.wrapper);
     this.wrapper.append(...this.items);
-    this.gallary.append(this.buttonPrev, this.buttonNext);
-    // console.log(this);
+    this.gallaryItems.append(this.buttonPrev, this.buttonNext);
   }
 
   createElement(tagName, options) {
@@ -164,3 +163,124 @@ menu.addEventListener('click', function(e){
     });
   }
 });
+
+const products =[
+  {
+    name: 'https://willpower.su/image/cache/catalog/sajt08.02.2020/vitamind32000iu120caps1-1200x800.jpg', 
+    price: 2000,
+    discont: 10
+},
+{
+  name: 'https://img.herbup.ru/images/products/1/782/696623886/8.jpg', 
+  price: 2000,
+  discont: 10
+},
+{
+  name: 'https://www.fitbynet.com/wp-content/uploads/2019/06/51cY2CZlMkL._SY679_.jpg', 
+  price: 2000,
+  discont: 10
+}
+] 
+  
+const server = {
+  cards(line = 1) {
+    const finished = line >= 1
+    const next = finished ? null : line + 1
+    const cards = Array(2).fill(products)
+    return new Promise((resolve) => {
+      
+      setTimeout(() => {
+        resolve({ cards, next })
+      }, 2000)
+    })
+  },
+}
+
+ async function checkPosition() {
+  const height = document.body.offsetHeight
+  const screenHeight = window.innerHeight
+  const scrolled = window.scrollY
+  const threshold = height - screenHeight/0.9
+  const position = scrolled + screenHeight
+  if (position >= threshold) {
+    await fetchData()
+  }
+}
+
+function throttle(callee, timeout) {
+  let timer = null
+
+  return function perform(...args) {
+    if (timer) return
+
+    timer = setTimeout(() => {
+      callee(...args)
+
+      clearTimeout(timer)
+      timer = null
+    }, timeout)
+  }
+}
+
+
+;(() => {
+  window.addEventListener('scroll', throttle(checkPosition, 250))
+  window.addEventListener('resize', throttle(checkPosition, 250))
+})()
+
+let nextPage = 1;
+let isLoading = false;
+let shouldLoad = true;
+
+async function fetchData() {
+  if (isLoading || !shouldLoad) return;
+  isLoading = true;
+  const { cards, next } = await server.cards(nextPage);
+  cards.forEach(appendData);
+  nextPage = next;
+  if (!next) shouldLoad = false;
+  isLoading = false;
+
+}
+
+function appendData(loadData) {
+  if (!loadData) return
+  const main = document.querySelector('.cards')
+  const cardNode = composeData(loadData)
+  main.append(cardNode)
+}
+
+function composeData(loadData) {
+  let block;
+  let key;
+  if (!loadData) return
+  const template = document.querySelectorAll('.card__item')
+  Array.from(template).forEach((file) => {
+    file.classList.add ('active');
+    preloader.classList.add('preloader--hide')
+  })
+  
+  let list = document.querySelector('.cards');
+  console.log(loadData);
+  
+  for (key in loadData) {
+  list.innerHTML = JSON.stringify(loadData)
+        `
+          <div class="card__item">
+              <div class="card__cover">
+              <img class="card__img" src=${loadData[key].name } alt="">
+              </div>
+              <div class="card__info">
+                  <p class="card__name">${loadData[key].price}</p>
+              </div>
+              <a href="#" class="card__button">Add to cart</a>
+          </div>
+        `
+  
+  }
+  return list;
+}
+
+
+
+
